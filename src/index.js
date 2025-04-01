6d4eae1bb4562b3693af44fb166f60796916ca8b
HelleFH <142264401+HelleFH@users.noreply.github.com>
2025-04-01 20:03:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for React 18
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // This is the new API for React 18:
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Instead of ReactDOM.render, use createRoot and render the app
 root.render(
   <React.StrictMode>
     <App />
